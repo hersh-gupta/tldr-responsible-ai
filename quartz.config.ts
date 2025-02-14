495539411ff746e42b8f54fe7eb474554a5ca5a0
hersh-gupta <h.v.gupta@outlook.com>
2025-02-14 16:41:02 -0500

    Quartz sync: Feb 14, 2025, 4:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     }, */
     locale: "en-US",
-    baseUrl: "https://hersh-gupta.github.io/tldr-responsible-ai",
+    baseUrl: "hersh-gupta.github.io/tldr-responsible-ai",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
